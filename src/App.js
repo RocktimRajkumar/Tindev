e8f854dee382aefdeec98dfff1251fc4db2df351
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-08-15 23:37:54 +0530

    initial commit: project structure created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rishtey Banao <span role="img" aria-label="heart">💘</span> </h1>
+    </div >
+  );
+}
+
+export default App;
